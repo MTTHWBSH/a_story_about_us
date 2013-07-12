49af97db044962233f216d341a27a704e15e9535
Josh D Compton <compto35@gmail.com>
2013-07-12 09:05:42 -0400

    Saving some of the more final things I've done to the first slide.

---
+++ _slides.php
@@ -0,0 +1,388 @@
+<?php
+$slides = array(
+//SLIDE 1
+  array(
+    'slide_bem' => 'classmates',
+    'slide_title' => 'Classmates',
+    'slide_content' => array(
+      array(
+        'identifier' => 'it-all-started',
+        'content' => 'For Matt and Kate, it all started in a large lecture hall at MSU. Though told by a mutual acquaintance they were perfect for each other, they somehow managed to avoid eye contact even once for the entire semester.',
+      ),
+      array(
+        'identifier' => 'final-day',
+        'content' => 'On the final day of class, with nothing to lose, Kate decided to make first contact&hellip;over the internet&hellip;inviting Matt to a Cinco de Mayo party.',
+      ),
+    ),//content
+
+    'slide_images' => array(
+      array(
+        'identifier' => 'matt-head',
+        'path' => 'img/slide-1/matt-head.png',
+      ),
+      
+      array(
+        'identifier' => 'kate-head',
+        'path' => 'img/slide-1/kate-head.png',
+      ),
+      
+      array(
+        'identifier' => 'class-scene',
+        'path' => 'img/slide-1/class-scene.png',
+      ),      
+    ),//images
+  ),//slide
+
+//SLIDE 2
+  array(
+    'slide_bem' => 'partiers',
+    'slide_title' => 'Partiers',
+    'slide_content' => array(
+      array(
+        'identifier' => 'drunkenly-biked',
+        'content' => 'On the chance of starting something special, <span class="tooltip" data-tooltip-title="Matt does not condone operation of bicycles while intoxicated">Matt drunkenly biked</span> across campus to attend the party. It was here that he realized he must not let this girl slip away.',
+      ),
+      array(
+        'identifier' => 'foreshadowing',
+        'content' => 'He had to see her again.',
+      ),
+    ),//content
+
+    'slide_images' => array(
+      array(
+        'identifier' => 'little-flags',
+        'path' => 'img/slide-2/banner.png',
+      ),
+      
+      array(
+        'identifier' => 'drunk-matt',
+        'path' => 'img/slide-2/matt.png',
+      ),
+      
+      array(
+        'identifier' => 'kate-couch',
+        'path' => 'img/slide-2/couch.png',
+      ),
+
+      array(
+        'identifier' => 'bar-bg',
+        'path' => 'img/slide-2/bar.png',
+      ),
+    ),//images
+  ),//slide
+
+//SLIDE 3
+  array(
+    'slide_bem' => 'roadies',
+    'slide_title' => 'Roadies',
+    'slide_content' => array(
+      array(
+        'identifier' => 'moved-home',
+        'content' => 'So Kate moved home for the summer, and Matt stayed in East Lansing. Weekend after weekend, the two took turns finding convenient excuses to be in the other’s cities.',
+      ),
+      array(
+        'identifier' => 'foreshadowing',
+        'content' => 'A friendship grew to the like that Matt had never experienced before.',
+      ),
+    ),//content
+
+    'slide_images' => array(
+      array(
+        'identifier' => 'beaumont',
+        'path' => 'img/slide-3/beaumont.png',
+      ),
+      
+      array(
+        'identifier' => 'caulder',
+        'path' => 'img/slide-3/caulder.png',
+      ),
+      
+      array(
+        'identifier' => 'el',
+        'path' => 'img/slide-3/el.png',
+      ),
+
+      array(
+        'identifier' => 'gr',
+        'path' => 'img/slide-3/gr.png',
+      ),
+
+      array(
+        'identifier' => 'michigan',
+        'path' => 'img/slide-3/michigan.png',
+      ),
+
+      array(
+        'identifier' => 'path1',
+        'path' => 'img/slide-3/path1.png',
+      ),
+      array(
+        'identifier' => 'path2',
+        'path' => 'img/slide-3/path2.png',
+      ),
+      array(
+        'identifier' => 'path3',
+        'path' => 'img/slide-3/path3.png',
+      ),
+      array(
+        'identifier' => 'path3',
+        'path' => 'img/slide-3/path3.png',
+      ),
+      array(
+        'identifier' => 'path4',
+        'path' => 'img/slide-3/path4.png',
+      ),
+      array(
+        'identifier' => 'path5',
+        'path' => 'img/slide-3/path5.png',
+      ),
+      array(
+        'identifier' => 'path6',
+        'path' => 'img/slide-3/path6.png',
+      ),
+      array(
+        'identifier' => 'path7',
+        'path' => 'img/slide-3/path7.png',
+      ),
+      array(
+        'identifier' => 'path8',
+        'path' => 'img/slide-3/path8.png',
+      ),
+    ),//images
+  ),//slide
+
+
+//SLIDE 4
+  array(
+    'slide_bem' => 'the-talk',
+    'slide_title' => 'The Talk',
+    'slide_content' => array(
+      array(
+        'identifier' => 'stolen-thunder',
+        'content' => 'So it was decided. On a weekend trip to Chicago, Matt would make this friendship a relationship, all he had to do was wait for the right moment…until of course Kate found the perfect moment herself—stealing Matt’s thunder.',
+      ),
+      array(
+        'identifier' => 'foreshadowing',
+        'content' => 'But no matter, this was the happiest Matt had ever been.',
+      ),
+    ),//content
+
+    'slide_images' => array(
+      array(
+        'identifier' => 'background',
+        'path' => 'img/slide-4/background.png',
+      ),
+      
+      array(
+        'identifier' => 'brick',
+        'path' => 'img/slide-4/brick.png',
+      ),
+      
+      array(
+        'identifier' => 'kate',
+        'path' => 'img/slide-4/kate.png',
+      ),
+
+      array(
+        'identifier' => 'matt',
+        'path' => 'img/slide-4/matt.png',
+      ),
+    ),//images
+  ),//slide
+
+//SLIDE 5
+  array(
+    'slide_bem' => 'happiness-graph',
+    'slide_title' => ':D/T',
+    'slide_content' => array(
+      array(
+        'identifier' => 'happiness-plus-plus',
+        'content' => 'To Matt’s surprise, things just kept getting better! Every moment spent with Kate made Matt happier and happier.',
+      ),
+      array(
+        'identifier' => 'foreshadowing',
+        'content' => 'He was sure this trend couldn’t get any better.',
+      ),
+    ),//content
+
+    'slide_images' => array(
+      array(
+        'identifier' => 'background',
+        'path' => 'img/slide-5/background.png',
+      ),
+      
+      array(
+        'identifier' => 'clock-bg',
+        'path' => 'img/slide-5/clock-bg.png',
+      ),
+      
+      array(
+        'identifier' => 'clock-face',
+        'path' => 'img/slide-5/clock-face.png',
+      ),
+
+      array(
+        'identifier' => 'clock-hand-hour',
+        'path' => 'img/slide-5/clock-hand-hour.png',
+      ),
+
+      array(
+        'identifier' => 'clock-hand-minute',
+        'path' => 'img/slide-5/clock-hand-minute.png',
+      ),
+
+      array(
+        'identifier' => 'dash-1',
+        'path' => 'img/slide-5/dash-1.png',
+      ),
+      array(
+        'identifier' => 'dash-2',
+        'path' => 'img/slide-5/dash-2.png',
+      ),
+      array(
+        'identifier' => 'dash-3',
+        'path' => 'img/slide-5/dash-3.png',
+      ),
+      array(
+        'identifier' => 'dash-4',
+        'path' => 'img/slide-5/dash-4.png',
+      ),
+      array(
+        'identifier' => 'dash-5',
+        'path' => 'img/slide-5/dash-5.png',
+      ),
+      array(
+        'identifier' => 'dash-6',
+        'path' => 'img/slide-5/dash-6.png',
+      ),
+      array(
+        'identifier' => 'dash-7',
+        'path' => 'img/slide-5/dash-7.png',
+      ),
+      array(
+        'identifier' => 'dash-8',
+        'path' => 'img/slide-5/dash-8.png',
+      ),
+      array(
+        'identifier' => 'dash-9',
+        'path' => 'img/slide-5/dash-9.png',
+      ),
+      array(
+        'identifier' => 'dash-10',
+        'path' => 'img/slide-5/dash-10.png',
+      ),
+      array(
+        'identifier' => 'dash-11',
+        'path' => 'img/slide-5/dash-11.png',
+      ),
+      array(
+        'identifier' => 'dash-12',
+        'path' => 'img/slide-5/dash-12.png',
+      ),
+      array(
+        'identifier' => 'dash-13',
+        'path' => 'img/slide-5/dash-13.png',
+      ),
+      array(
+        'identifier' => 'dash-14',
+        'path' => 'img/slide-5/dash-14.png',
+      ),
+
+      array(
+        'identifier' => 'happy-face',
+        'path' => 'img/slide-5/happy-face.png',
+      ),
+    ),//images
+  ),//slide
+
+//SLIDE 6
+  array(
+    'slide_bem' => 'family-plus-plus',
+    'slide_title' => 'Family++',
+    'slide_content' => array(
+      array(
+        'identifier' => 'along-came-margot',
+        'content' => 'Then along came Margot. Margot proved to be the finest addition to their little family Matt could hope for.',
+      ),
+      array(
+        'identifier' => 'setup',
+        'content' => 'He certainly couldn’t imagine anything that would make him happier&hellip;',
+      ),
+      array(
+        'identifier' => 'foreshadowing',
+        'content' => '&hellip;than making this last a lifetime.',
+      ),
+    ),//content
+
+    'slide_images' => array(
+      array(
+        'identifier' => 'kate-head',
+        'path' => 'img/slide-6/kate-head.png',
+      ),
+      
+      array(
+        'identifier' => 'matt-head',
+        'path' => 'img/slide-6/matt-head.png',
+      ),
+      
+      array(
+        'identifier' => 'margot-head',
+        'path' => 'img/slide-6/margot-head.png',
+      ),
+
+      array(
+        'identifier' => 'margot-paw-left',
+        'path' => 'img/slide-6/margot-paw-left.png',
+      ),
+
+      array(
+        'identifier' => 'margot-paw-right',
+        'path' => 'img/slide-6/margot-paw-right.png',
+      ),
+    ),//images
+  ),//slide
+
+//SLIDE 7
+  array(
+    'slide_bem' => 'city-bound',
+    'slide_title' => 'City Bound',
+    'slide_content' => array(
+      array(
+        'identifier' => 'graduation',
+        'content' => 'Suddenly, the world was flipped upside down by some pesky business involving graduation. It was time to enter the real world.',
+      ),
+      array(
+        'identifier' => 'foreshadowing',
+        'content' => 'And so with both having dreams of living in Chicago and both knowing they couldn’t do it without each other, they packed their bags and headed for the next chapter.',
+      ),
+    ),//content
+
+    'slide_images' => array(
+      array(
+        'identifier' => 'bean',
+        'path' => 'img/slide-7/bean.png',
+      ),
+      
+      array(
+        'identifier' => 'car',
+        'path' => 'img/slide-7/car.png',
+      ),
+      
+      array(
+        'identifier' => 'road',
+        'path' => 'img/slide-7/road.png',
+      ),
+
+      array(
+        'identifier' => 'skyscraper',
+        'path' => 'img/slide-7/skyscraper.png',
+      ),
+
+      array(
+        'identifier' => 'willis-tower',
+        'path' => 'img/slide-7/willis-tower.png',
+      ),
+    ),//images
+  ),//slide
+
+);//slides
--- js/love-story.js
+++ js/love-story.js
@@ -19,48 +19,57 @@ var loveStory = {
     self.pointer = 0;
     
     //set our event listeners for the keys
-/*
-    $(document).keypress(function(event){
+    $(document).keydown(function(e){
+      var keycode = e.keyCode || e.keycode || e.which;
+      var keyLeft = 37;
+      var keyRight = 39;
+      var spaceBar = 32;
       
-      var keyLeft = 12;
-      var keyRight = 13;
-      var spaceBar = 14;
-      
-      //left key
-      if( event.keycode === keyLeft ){
-        event.preventDefault();
-        //if the pointer is at 1 or 0, just stop
-        if(self.pointer <= 1){
-          self.pointer = 0;
-          return;
-        }
-        
-        //every animation ends with incrementing the pointer, so to go back we need to minus by 2
-        self.pointer - 2;
-        self.index(self.pointer);
-      }//if e.keycode
-      
-      //right key and space bar
-      if( event.keycode === keyRight ||
-          event.keycode === spaceBar ){
-        event.preventDefault();
-        
-      }
+      switch(keycode){
+        case keyLeft:
+          e.preventDefault();
+
+          //if the pointer is at 1 or 0, just stop
+          if(self.pointer <= 1){
+            self.pointer = 0;
+            return;
+          }
+          
+          //every animation ends with incrementing the pointer, so to go back we need to minus by 2
+          self.pointer - 2;
+          self.index(self.pointer);
+
+          break;
+        case keyRight || spaceBar:
+          e.preventDefault();
+          self.index(self.pointer);
+          break;
+      }//switch
+
     });
-*/
     
-    //hide everything and call the index
+    //call the index
     var frame = $('.story_book');
     var intro = frame.find('.story_book__intro');
-    frame.children().hide();
+    self.hide_this(frame.children('.slide'));
     self.limbo(self.pointer);
   },
   
+  //expects a jquery object
+  hide_this: function(this_guy){
+    this_guy.removeClass('visible');
+  },//hide_this
+  //expects a jquery object
+  show_this: function(this_guy){
+    this_guy.addClass('visible');
+  },//show_this()
   
   /**
     limbo()
     --
-    clear the stage between pages or non-sequential transitions, put in a spinner, and call the index with the proper animation
+    clear the stage between pages or non-sequential transitions, put in a spinner, and call the
+      index with the proper animation. It's assumed that the first animation of the chapter will 
+      show the slide, title, and whatever else
     
     @param int page | define the page we're transitioning /to/
     @return void
@@ -69,7 +78,7 @@ var loveStory = {
     var self = this;
     //hide everything and call the index with the current animation
     var frame = $('.story_book');
-    if(pointer > 0){
+    if(pointer >= 0){
       frame.children().fadeOut(500);
     }
     
@@ -89,11 +98,20 @@ var loveStory = {
   index : function(pos){
     var self = this;
     var frame = $('.story_book');
-
+    //chapter selectors
+      var intro = frame.find('.story_book__intro'),
+          slideOne = frame.find('.slide.classmates'),
+          slideTwo = frame.find('.slide.partiers')
+          slideThree = frame.find('.slide.roadies'),
+          slideFour = frame.find('.slide.the-talk'),
+          sldeFive = frame.find('.slide.happiness-graph'),
+          slideSix = frame.find('.slide.family-plus-plus'),
+          slideSeven = frame.find('.slide.city-bound'),
+          colophon = frame.find('.story_book__colophon');
+      
     switch(pos){
 
       case 0 :
-        var intro = frame.find('.story_book__intro').fadeIn(800);
         var enterButton = intro.find('.story_book__open');
         //when the enter button is clicked, set the pointer to 1, and call limbo (start of chapter 1)
         $(enterButton).click(function(){
@@ -101,14 +119,14 @@ var loveStory = {
           self.limbo(self.pointer);
         });
         intro.fadeIn(800);
+        
         break;
       
       //CHAPTER 1
       
       //bring in the title and the first paragraph
       case 1 : 
-        var slideOne = frame.find('.slide.classmates');
-        var slideTitle = slideOne.find('.slide_title');
+        var slideTitle = slideOne.find('.slide__title');
         var slideP1 = slideOne.find('.classmates__it-all-started');
         
         //hide everything, position the title, and show stuff
@@ -116,16 +134,42 @@ var loveStory = {
         slideOne.show(0);
         
         //animate the title
-        slideTitle.show(0).css({
-          display : 'block',
-          right: '-20px'
-        }, 400);
+        slideTitle.show(0).animate({
+          right: '-2%'
+        }, 1000, function(){
+          slideP1.fadeIn(400);
+        });
         
-        slideP1.fadeIn(400);
+        break;
+      case 2:
+        var classScene = slideOne.find('.classmates__class-scene');
+        var matt = slideOne.find('.classmates__matt-head');
+        var kate = slideOne.find('.classmates__kate-head');
+        var lastP = slideOne.find('.classmates__final-day');
+        
+        classScene.fadeIn(800, function(){
+          matt.show(0).animate({
+            top: 130
+          }, 900);
+
+          kate.show().animate({
+            top: 167
+          },900, function(){
+              lastP.fadeIn(700);
+            });
+        });
+        break;
+      case 3:
+        var slideTitle = slideTwo.find('.slide__title');
+        var slideP1 = slideTwo.find('.partiers__foreshadowing');
+        
+        self.limbo(self.pointer);
+        slideTwo.show(0);
         
         break;
     }//switch
-    
+    //increment our pointer
+    self.pointer++;
   }//index()
   
 };//loveStory
